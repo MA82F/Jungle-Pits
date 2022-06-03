2461460d080b4c72300b9721fec28ce4e95241a0
MohammadAmin <fahimi13823182@gmail.com>
2022-06-03 13:59:20 +0430

    make n/2 (domestic & wild) and 1000 movement

--- main.cpp
+++ main.cpp
@@ -5,21 +5,114 @@
 #include "WildAnimal.cpp"
 using namespace std;
 int main() {
-    // int numOfAnimals;
-    // cin>>numOfAnimals;
-    // Jungle forest(numOfAnimals);
-    // if (numOfAnimals%2==1){
-    //     for (int i = 0; i < numOfAnimals/2+1; i++)
-    //     {
-    //         forest.AddAnimal(new DomesticAnimal());
-    //         forest.AddAnimal(new WildAnimal());
-    //     }
-    //     WildAnimal wild(numOfAnimals/2);
-    // }
-    // else{
-    //     DomesticAnimal domestic(numOfAnimals/2);
-    //     WildAnimal wild(numOfAnimals/2);
-    // }
-
+    int numOfAnimals;
+    cin>>numOfAnimals;
+    Jungle forest(numOfAnimals);
+    for (int i = 0; i < numOfAnimals/2; i++){
+        forest.AddAnimal(new DomesticAnimal());
+        forest.AddAnimal(new WildAnimal());
+    }
+    forest.AddAnimal(new DomesticAnimal());
+    forest.movement();              //1000 movement
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();
+    forest.movement();    //1000 movement
+    cout<<"Total Fell Animals in Pits!!: "<< forest.getTotalFall()<<endl;
     return 0;
 }
