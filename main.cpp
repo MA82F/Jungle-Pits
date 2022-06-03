bfec6720ec1cbd9e91edbf0b9992258ae0897cd9
MohammadAmin <fahimi13823182@gmail.com>
2022-06-03 13:41:38 +0430

    main.cpp is starting .

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Jungle.cpp"
+#include "Animal.cpp"
+#include "DomesticAnimal.cpp"
+#include "WildAnimal.cpp"
+using namespace std;
+int main() {
+    // int numOfAnimals;
+    // cin>>numOfAnimals;
+    // Jungle forest(numOfAnimals);
+    // if (numOfAnimals%2==1){
+    //     for (int i = 0; i < numOfAnimals/2+1; i++)
+    //     {
+    //         forest.AddAnimal(new DomesticAnimal());
+    //         forest.AddAnimal(new WildAnimal());
+    //     }
+    //     WildAnimal wild(numOfAnimals/2);
+    // }
+    // else{
+    //     DomesticAnimal domestic(numOfAnimals/2);
+    //     WildAnimal wild(numOfAnimals/2);
+    // }
+
+    return 0;
+}
